a7dff29d6ebd36080635aa3964c5327730b64ee6
InterCalm <C_Jesse93@yahoo.com>
2016-01-07 16:29:52 -0800

    Test commit

---
+++ Assets/Scripts/MainCharacterControl.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCharacterControl : MonoBehaviour {
+	float speed=1.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var move = Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+		transform.position += move * speed * Time.deltaTime;
+	}
+	}
+
---
+++ README.txt
@@ -0,0 +1,12 @@
+Development game 
+made by Jesse Chhun 
+Started on Jan 7 2016
+
+
+Inital game to test github 
+and unity game
+
+
+Game artwork are from the following people:
+Character model:
+platforms: 
