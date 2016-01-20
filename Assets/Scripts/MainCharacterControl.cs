44985bb1d6b8f10ba02bb1a077971b5eefe42172
Jesse Chhun <C_Jesse93@yahoo.com>
2016-01-20 15:13:50 -0800

    fixing player bugs
    debug Unity errors when opening files
    updates to player script
    updates to test level

--- Assets/Scripts/MainCharacterControl.cs
+++ Assets/Scripts/MainCharacterControl.cs
@@ -2,7 +2,11 @@
 using System.Collections;
 
 public class MainCharacterControl : MonoBehaviour {
-	float speed=1.0f;
+	float speed=7.0f;
+	int health=3; 
+	float DirY;
+	float DirX;
+	Animator anime;
 	// Use this for initialization
 	void Start () {
 	
@@ -10,8 +14,14 @@ public class MainCharacterControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		var move = Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+		var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
 		transform.position += move * speed * Time.deltaTime;
+	
+		if (Input.GetAxis ("Horizontal") != 0) {
+			GetComponent<Animator>().SetFloat("DirX", move.x);
+		}
+
 	}
+
 	}
 
--- README.txt
+++ README.txt
@@ -6,7 +6,11 @@ Started on Jan 7 2016
 Inital game to test github 
 and unity game
 
-
+Updates:
+Jan 20
+v1.0- fix errors when sharing and sending unity project
+    - changed main character in test scene so character is now able to move
+   
 Game artwork are from the following people:
 Character model:
 platforms: 
