bc184663de1eec07d7f05e5a7d4b8304b3bec946
Jesse Chhun <C_Jesse93@yahoo.com>
2016-01-20 18:06:16 -0800

    Script update to make the animation smoother
    cleaner flip animation

--- Assets/Scripts/MainCharacterControl.cs
+++ Assets/Scripts/MainCharacterControl.cs
@@ -7,29 +7,37 @@ public class MainCharacterControl : MonoBehaviour {
 	float DirY;
 	float DirX;
 	Animator anime;
+	bool attack=false;
+	bool characterFacingRight=true;
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
 		transform.position += move * speed * Time.deltaTime;
 	
+
+		if (Input.GetAxis("Horizontal") > 0 && !characterFacingRight) 
+		{
+			flip();
+		} 
+		else if (Input.GetAxis ("Horizontal") < 0 && characterFacingRight) 
+		{
+			flip();
+		}
 		if (Input.GetAxis ("Horizontal") > 0) {
 			GetComponent<Animator>().SetFloat("DirX", move.x);
 		}
-		if (Input.GetAxis ("Horizontal") < 0) {
-			flip ();
-			GetComponent<Animator>().SetFloat("DirX",move.x);
-
-		}
-
 	}
 	void flip(){
-	Vector3 theScale = transform.localScale;
-	theScale.x *= -1;
-	transform.localScale = theScale;
+		// Set Character Facing Direction To Opposite or False
+		characterFacingRight = !characterFacingRight;
+		Vector3 currentScale = transform.localScale;
+		// Multiply Current Scale By Negative To Flip Direction
+		currentScale.x *= -1;
+		transform.localScale = currentScale;
 	}
 }
